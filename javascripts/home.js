53aaa9ccad97b20ab7ba9a64c44275e639502f92
toshik333 <toshik.smirnov@gmail.com>
2023-06-03 00:41:48 +0700

    final commit

---
+++ javascripts/home.js
@@ -0,0 +1,14 @@
+
+var modal = document.getElementById('id01');
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
